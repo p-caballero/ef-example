2b83a325ae2003df95cca15cc5d1cc7ec7dea9f1
Pablo Caballero <deidigitalis@gmail.com>
2022-10-07 20:13:54 +0200

    docs: ejemplo 1 y 2.

---
+++ capturas/ejemplo-1.cs
@@ -0,0 +1,6 @@
+Func<int,int> miFuncion = v =>
+{
+    return v*v;
+};
+
+int miValor = miFuncion(33);
---
+++ capturas/ejemplo-2.cs
@@ -0,0 +1,21 @@
+﻿namespace EfExample
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = MiFuncion(x => x + 10, () =>
+            {
+                Console.WriteLine("HOLA MUNDO!");
+            });
+        }
+
+        private static int MiFuncion(Func<int,int> v, Action action)
+        {
+            action(); // action.Invoke();
+            return v(33);
+        }
+    }
+}
